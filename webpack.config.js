3d42b628c053fb3befba28eb96fc89ada5b72113
skang1004 <saejin.kang1004@gmail.com>
2020-07-15 15:35:56 -0700

    setting up initial boilerplate for recoilize landing page

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    publicPath: '/build',
+    historyApiFallback: true,
+    hot: true,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
